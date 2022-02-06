e31d1bae4805e2dcffcf6bb9988e4921c1eb35cb
David Camp <75981851+dave-vertias@users.noreply.github.com>
2022-02-06 10:07:52 -0500

    Add files via upload

--- hangman.c
+++ hangman.c
@@ -2,7 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 #include <curl/curl.h>
+#include <math.h>
+#include <unistd.h>
 
+#define pi 3.142857
+
+int contains(char entered, char *word, char *hidden_word, int *win_count){
+    int c = 0;
+    for(int i = 0; i < strlen(word); i++){
+        if(word[i] == entered){
+            hidden_word[i] = word[i];
+            c++;
+            (*win_count)++;
+        }
+    }
+    return c;
+}
 
 //hangman game
 int main(int argc, char **argv){
@@ -27,8 +42,9 @@ int main(int argc, char **argv){
 
     result = curl_easy_perform(curl);
 
-    if(result == CURLE_OK)
-        printf("Download Complete\n\n");
+    if(result == CURLE_OK){
+        result = 0;
+    }
     else
         printf("Error: %s\n\n", curl_easy_strerror(result));
 
@@ -54,17 +70,114 @@ int main(int argc, char **argv){
         fclose (f);
     }
 
-    printf("%s\n\n",buffer);
-
     int count = 0;
+    int win_count = 0;
     for(int i = 9, j = 0; i < strlen(buffer)-1; i++, j++){
         word[j] = buffer[i];
         count++;
     }
     word[count-1] = 0;
-    printf("%s\n\n",word);
+    if(word[count-2] == '"')
+        word[count-2] = 0;
 
-    //section 3 Graphics and game itself. 
+    count = 0;
+    char hidden_word[strlen(word)];
 
+    for(int i = 0; i < strlen(word); i++){
+        hidden_word[i] = '*';
+    }
+    hidden_word[strlen(word)] = 0;
+
+    //section 3 Graphics and game itself-------------------------------------------------
+
+    char guessing = 0;
+    while(1){
+        system("clear");
+        printf("\n\n====================================\n");
+        printf("|       Welcome To HangMan         |\n");
+        printf("====================================\n\n");
+
+        printf("\n________________\n");
+        //rope
+        if (count==0)
+        printf("|          \n"
+               "|          \n"
+               "|         \n"
+               "|          \n"
+               "|          \n"
+               "|           \n");
+
+        else if (count==1)
+        printf("|          |\n"
+               "|          \n"
+               "|         \n"
+               "|          \n"
+               "|          \n"
+               "|           \n");
+        else if (count==2)
+        printf("|          |\n"
+               "|          O\n"
+               "|         \n"
+               "|          \n"
+               "|          \n"
+               "|           \n");
+        else if (count==3)
+        printf("|          |\n"
+               "|          O\n"
+               "|          |\n"
+               "|          \n"
+               "|          \n"
+               "|           \n");
+        else if (count==4)
+        printf("|          |\n"
+               "|          O\n"
+               "|         /|\n"
+               "|          \n"
+               "|          \n"
+               "|           \n");
+        else if (count==5)
+        printf("|          |\n"
+               "|          O\n"
+               "|         /|\\\n"
+               "|          \n"
+               "|          \n"
+               "|           \n");
+        else if (count==6)
+        printf("|          |\n"
+               "|          O\n"
+               "|         /|\\\n"
+               "|          /\n"
+               "|          \n"
+               "|           \n");
+        else if (count>=7){
+        
+        printf("|          |\n"
+               "|          O\n"
+               "|         /|\\\n"
+               "|          /\\\n"
+               "|          \n"
+               "|           \n");
+        printf("GAME OVER! YOU LOST!\n\n");
+        printf("Word: %s\n\n",word);
+        exit(0);
+        }
+        if(win_count == (int)strlen(word)){
+            system("clear");
+            printf("\n\nYOU WIN!!\n\n");
+            printf("\nWord: %s\n\n", word);
+            exit(0);
+        }
+
+        printf("Word to Guess: %s\n", hidden_word);
+        printf("\n\nEnter a character to guess: ");
+        scanf(" %c",&guessing);
+        printf("\n");
+
+        if(contains(guessing, word, hidden_word, &win_count) == 0){
+            count++;
+        } 
+        
+        system("clear");
+    }
     return 0;
     }
